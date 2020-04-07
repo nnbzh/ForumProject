6464b91f502636de82113871c86ab4cec86e5a5f
Nurassyl Balgabay <nurasyl.bbn@gmail.com>
2020-04-08 03:04:22 +0600

    get categories and its discussions

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { MainComponent } from './main/main.component';
 
 
 const routes: Routes = [
-  { path: '', component: MainComponent}
+  { path: '', component: MainComponent},
 ];
 
 @NgModule({
