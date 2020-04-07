7d7af904c1499e90e1436db2d3c211a8c151c937
KaraKala1427 <ernarerbol027@gmail.com>
2020-04-08 03:22:08 +0600

    login demopage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 
 
 const routes: Routes = [
-  { path: '', component: MainComponent}
+  { path: 'main', component: MainComponent},
+  { path: '', redirectTo: '/main', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { MainComponent } from './main/main.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    MainComponent
+    MainComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
